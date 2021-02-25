c7789295fef097b50a2cd5acb938a83726151595
Ladanovasv <ladanova.sv@mail.ru>
2021-02-25 19:13:11 +0300

    ScrollView

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import  React from "react"
-import {View} from "react-native";
+import {ScrollView} from "react-native";
 
 class App extends React.Component{
   render(){
     return(
-      <View/>
+      <ScrollView/>
     )
   }
 }
