7656613a107b44ad99c725a15dca82999374094d
k1_502_11 <k1_502_11@syktsu.ru>
2021-02-25 18:51:44 +0300

    Wiew

--- App.js
+++ App.js
@@ -4,16 +4,7 @@ import {View, Text} from "react-native";
 class App extends React.Component{
   render(){
     return(
-      <View
-        style = {{
-          flex:1,
-          justifyContent:"center",
-          alignItems:"center"
-        }}
-        > 
-        <Text> Hello world! Git! </Text>
-        <Text> Hello world! Git! </Text>
-      </View>
+      <View/>
     )
   }
 }
