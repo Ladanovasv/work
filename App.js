b1422b1142f0dfe012b1d2900783afd0a113eecb
Svetlana <47398458+Ladanovasv@users.noreply.github.com>
2021-02-25 18:52:58 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import  React from "react"
-import {View, Text} from "react-native";
+import {ScrollView, Text} from "react-native";
 
 class App extends React.Component{
   render(){
     return(
-      <View
-        style = {{
-          flex:1,
-          justifyContent:"center",
-          alignItems:"center"
-        }}
-        > 
-        <Text> Hello world! Git! </Text>
-        <Text> Hello world! Git! </Text>
-      </View>
+      <ScrollView/>
     )
   }
 }
