6f065422713c100336442f626cbe9a55fc625884
Ladanovasv <ladanova.sv@mail.ru>
2021-03-28 12:53:00 +0300

    Fix bag

---
+++ src/components/AddProductForm.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+
+class AddProductForm extends React.Component {
+  render() {
+    const {addProduct, updateNameProduct} = this.props;
+    const {height} = Dimensions.get('window');
+
+    onPress = () => {
+      addProduct();
+    };
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          height: height * 0.09,
+          backgroundColor: '#fafafa',
+          padding: 5,
+          elevation: 18,
+          zIndex: 1,
+          borderRadius: 10,
+        }}>
+        <TextInput
+          style={{
+            flex: 6,
+            borderRadius: 25,
+            paddingLeft: 25,
+            paddingRight: 25,
+            marginHorizontal: 10,
+            fontSize: 14,
+            backgroundColor: '#ffffff',
+            borderColor: '#ececec',
+            borderWidth: 1,
+          }}
+          onChangeText={updateNameProduct}
+        />
+
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: 50,
+            borderRadius: 25,
+            backgroundColor: '#f75d5d',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={onPress}>
+          <Text
+            style={{
+              fontSize: 30,
+              textAlign: 'center',
+              color: 'white',
+            }}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default AddProductForm;
